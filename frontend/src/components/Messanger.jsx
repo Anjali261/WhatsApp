9f19aee3f4e43b9fc950512fe6c8d4098f667e31
Anjali Singh <anjalisingh@Anjalis-MacBook-Air.local>
2023-09-05 01:21:28 +0530

    UI for WhatsApp Login

--- frontend/src/components/Messanger.jsx
+++ frontend/src/components/Messanger.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import {AppBar, Box,Toolbar,styled} from '@mui/material'
 import LoginDialog from './account/LoginDialog'
+
+const Component = styled(Box)`
+height:100vh;
+background-color: #DCDCDC;
+`
+
+const Header = styled(AppBar)`
+height:220px;
+background-color: #00bfa5;
+box-shadow:none;
+`
+
 
 const Messanger = () => {
   return (
+    <>
+    <Component>    
+  <Header>
+    <Toolbar>
+
+    </Toolbar>
+    </Header>
     <LoginDialog/>
+    </Component>
+      </>
   )
 }
 
--- frontend/src/components/account/LoginDialog.jsx
+++ frontend/src/components/account/LoginDialog.jsx
@@ -1,8 +1,73 @@
+import { Dialog,Box, Typography, ListItem , List, styled} from '@mui/material'
+import shadows from '@mui/material/styles/shadows'
 import React from 'react'
+import {qrCodeImage} from '../../constants/data'; 
+const Component = styled(Box)`
+display:flex;
+
+`
+const Container = styled(Box)`
+padding:56px 0 56px 56px;
+
+`
+const QRCode = styled('img')({
+    height:264,
+    width:264,
+    margin:'50px 0 0 50px'
+} 
+)
+const Title = styled(Typography)`
+font-size:25px;
+color:#525252;
+font-weight:300;
+font-family:inherit;
+margin-bottom:25px;
+`
+const styleList = styled(List)`
+& > li {
+    padding:0;
+    margin-top: 15px;
+    font-sixe:18px;
+    line-height:28px;
+    color: #4a4a4a;
+    
+}
+`
+
+
+const dialogStyle ={
+    marginTop:'12%',
+    height: '96%',
+    width:'60%',
+    maxWidth:'100%',
+    maxHeight:'100%',
+    boxShadow:'none',
+    overflow:'hidden',
+}
 
 const LoginDialog = () => {
   return (
-    <div>LoginDialog</div>
+    <Dialog open={true}
+    PaperProps={{sx:dialogStyle}}>
+
+    <Component>
+        <Container>
+        <Title>
+            To use Whatsaap on Your Computer
+        </Title>
+        <styleList>
+            <ListItem>1. Open WhatsApp on Your Computer</ListItem>
+            <ListItem>2.Tap Menu : or Setting and Select LinkedDevices</ListItem>
+            <ListItem>
+                3.Point your Phone to this screen to capture the code
+            </ListItem>
+        </styleList>
+        </Container>
+        <Box>
+            <QRCode src={qrCodeImage} alt="qr code" />
+        </Box>
+    </Component>
+    </Dialog>
   )
 }
 
