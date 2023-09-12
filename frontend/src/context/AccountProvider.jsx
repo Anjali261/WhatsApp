1a61174797fab8282e742d131c350cb9e57ea56a
Anjali261 <mail2anjali.singh2610@gmail.com>
2023-09-13 02:32:11 +0530

    AddedAddUser Functionality in Socket

--- frontend/src/context/AccountProvider.jsx
+++ frontend/src/context/AccountProvider.jsx
@@ -1,4 +1,6 @@
-import { createContext , useState } from "react";
+import { createContext , useState ,useRef , useEffect} from "react";
+
+import { io } from 'socket.io-client';
 
 export const AccountContext = createContext( null);
 
@@ -7,12 +9,22 @@ const AccountProvider = ( { children}) =>{
 //store inf of login account globally
 const [account,setAccount] = useState();
 const [person,setPerson] = useState({});
+
+
+//establish socket.io through useRef
+const socket = useRef();
+//estalish socket
+useEffect(()=>{
+    socket.current = io('ws://localhost:9000')
+},[])
+
     return(
         <AccountContext.Provider value={{
             account, 
             setAccount,
             person,
-            setPerson
+            setPerson,
+            socket
 
         }}>
             {children}
--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,23 @@
-import { server} from "socket.io";
+import { Server} from "socket.io";
 const io = new Server(9000, {
     cors:{
         origin:"http://localhost:3000",
     }
 
 })
+//active user
+let users=[];
+
+const addUser = (userData,socketId) =>{
+    //
+    !user.some(user => user.sub == userData.sub) && (users.push({...userData,socketId}))
+
+}
 
 io.on('connection', (socket) =>{
     console.log('user connected');
+
+    socket.on("addUser", userData =>{
+        addUser(userData , socket.id);
+    })
 })
